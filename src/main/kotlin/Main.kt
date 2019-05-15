0e0b63de6566a094c8913345df7f39dfb95a7778
borisov <rundoom337@gmail.com>
2019-05-15 15:55:02 +0400

    Main class fixed

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,3 @@
-import java.io.File
-
 fun main() {
-    File("D:\\IdeaProjects\\BeliefBlinker\\src\\main\\web\\fonts").walk()
-        .forEach {
-            print("\"${it.nameWithoutExtension}\",")
-        }
+    initServer()
 }
